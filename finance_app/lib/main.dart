39351cfbd8cd3bb6c4b3aa14c3a87f69e0c371cf
anurag67124 <at9369067124@gmail.com>
2023-02-02 19:47:23 +0530

    the first flutter app

---
+++ finance_app/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:finance_app/Screens/home.dart';
+import 'package:finance_app/Screens/statistics.dart';
+import 'package:finance_app/widgets/bottomnavigationbar.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'data/model/add_date.dart';
+
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(AdddataAdapter());
+  await Hive.openBox<Add_data>('data');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Bottom(),
+    );
+  }
+}
---
+++ finance_app/lib/widgets/bottomnavigationbar.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:finance_app/Screens/add.dart';
+import 'package:finance_app/Screens/home.dart';
+import 'package:finance_app/Screens/statistics.dart';
+
+class Bottom extends StatefulWidget {
+  const Bottom({Key? key}) : super(key: key);
+
+  @override
+  State<Bottom> createState() => _BottomState();
+}
+
+class _BottomState extends State<Bottom> {
+  int index_color = 0;
+  List Screen = [Home(), Statistics()];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Screen[index_color],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => Add_Screen()));
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Color.fromARGB(255, 52, 3, 62),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 7.5, bottom: 7.5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    index_color = 0;
+                  });
+                },
+                child: Icon(
+                  Icons.home,
+                  size: 30,
+                  color: index_color == 0
+                      ? Color.fromARGB(255, 128, 4, 150)
+                      : Colors.black,
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    index_color = 1;
+                  });
+                },
+                child: Icon(
+                  Icons.bar_chart_outlined,
+                  size: 30,
+                  color: index_color == 1
+                      ? Color.fromARGB(255, 128, 4, 150)
+                      : Colors.black,
+                ),
+              ),
+              SizedBox(width: 10),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    index_color = 2;
+                  });
+                },
+                child: Icon(
+                  Icons.account_balance_wallet_outlined,
+                  size: 30,
+                  color: index_color == 2
+                      ? Color.fromARGB(255, 128, 4, 150)
+                      : Colors.black,
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    index_color = 3;
+                  });
+                },
+                child: Icon(
+                  Icons.person_outlined,
+                  size: 30,
+                  color: index_color == 3
+                      ? Color.fromARGB(255, 128, 4, 150)
+                      : Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
