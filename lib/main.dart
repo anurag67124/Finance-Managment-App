2b1b2e249ea6b70fcd0d8ace22a4e57fc434c9ae
Mark Stephen Thomas <markstephenthomas10@gmail.com>
2023-02-08 16:28:02 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:financial_app/screens/login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'Poppins'),
+      home: LogInScreen(),
+    );
+  }
+}
---
+++ lib/screens/signup.dart
@@ -0,0 +1,99 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:financial_app/screens/login.dart';
+import 'package:financial_app/theme.dart';
+import 'package:financial_app/widgets/primary_button.dart';
+import 'package:financial_app/widgets/checkbox.dart';
+import 'package:financial_app/widgets/signup_form.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+
+class SignUpScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 70,
+            ),
+            Padding(
+              padding: kDefaultPadding,
+              child: Text(
+                'Create Account',
+                style: titleText,
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Padding(
+              padding: kDefaultPadding,
+              child: Row(
+                children: [
+                  Text(
+                    'Already a member?',
+                    style: subTitle,
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => LogInScreen()));
+                    },
+                    child: Text(
+                      'Log In',
+                      style: textButton.copyWith(
+                        decoration: TextDecoration.underline,
+                        decorationThickness: 1,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: kDefaultPadding,
+              child: SignUpForm(),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: kDefaultPadding,
+              child: CheckBox('Agree to terms and conditions.'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            SignInButton(
+              padding: kDefaultPadding,
+              Buttons.Google,
+              text: "OR Sign up with Google",
+              onPressed: () {},
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: kDefaultPadding,
+              child: PrimaryButton(buttonText: 'Sign Up'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
