26c9d126a0c87cce819c2b9ddaaa2d7fe080abad
Mark Stephen Thomas <markstephenthomas10@gmail.com>
2023-02-24 11:49:42 +0530

    login_screen added

---
+++ finance_app/lib/Screens/login.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:finance_app/screens/reset_password.dart';
+import 'package:finance_app/screens/signup.dart';
+import 'package:finance_app/theme.dart';
+import 'package:finance_app/widgets/login_form.dart';
+import 'package:finance_app/widgets/primary_button.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+
+class LogInScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: kDefaultPadding,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 120,
+              ),
+              Text(
+                'Welcome Back',
+                style: titleText,
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Row(
+                children: [
+                  Text(
+                    'New to this app?',
+                    style: subTitle,
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SignUpScreen(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      'Sign Up',
+                      style: textButton.copyWith(
+                        decoration: TextDecoration.underline,
+                        decorationThickness: 1,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              LogInForm(),
+              SizedBox(
+                height: 20,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ResetPasswordScreen()));
+                },
+                child: Text(
+                  'Forgot password?',
+                  style: TextStyle(
+                    color: kZambeziColor,
+                    fontSize: 14,
+                    decoration: TextDecoration.underline,
+                    decorationThickness: 1,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SignInButton(
+                padding: EdgeInsets.symmetric(),
+                Buttons.Google,
+                text: "OR Sign in with Google",
+                onPressed: () {},
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              PrimaryButton(
+                buttonText: 'Log In',
+              ),
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- finance_app/lib/main.dart
+++ finance_app/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:finance_app/Screens/home.dart';
 import 'package:finance_app/Screens/statistics.dart';
 import 'package:finance_app/widgets/bottomnavigationbar.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-
+import 'package:finance_app/screens/login.dart';
 import 'data/model/add_date.dart';
 
 void main() async {
@@ -15,7 +15,7 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-   static Color my_color = Color.fromARGB(255, 128, 4, 150);
+  static Color my_color = Color.fromARGB(255, 128, 4, 150);
 
   @override
   Widget build(BuildContext context) {
@@ -23,9 +23,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Bottom(),
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        buttonTheme: ButtonThemeData(buttonColor:my_color )
-      ),
+          primarySwatch: Colors.deepPurple,
+          buttonTheme: ButtonThemeData(buttonColor: my_color)),
     );
   }
 }
